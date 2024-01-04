35d7bf21fda600e9b7f95ec2dff8ac8fb54106a3
carolinenunes <carolineonunes8@gmail.com>
2024-01-03 21:06:05 -0300

    novas funcionalidades

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,20 +1,51 @@
 let numeroSecreto = gerarNumeroAleatorio();
-
+let tentativa = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function mensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+mensagemInicial();
+
 
 function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
-    console.log(numeroSecreto);
+    let chute = document.querySelector('input').value; //input é um campo de entrega do que o usuário escreve. value é o valor do campo, ou seja, o que o usuário digitou no campo.
+    
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativa > 1 ? 'tentativas' :  'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativa} ${palavraTentativa}! Clique no botão "Novo Jogo" para jogar novamente.`;
+        exibirTextoNaTela('h1', 'Você acertou!' );
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativa++
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = 5;
+    limparCampo();
+    mensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() *10 + 1);
+    return 5 //parseInt(Math.random() *10 + 1);
 }
